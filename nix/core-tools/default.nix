04891c2255843db13b9b1bb062bc4b1a559bc31a
futtetennista <futtetennista@pm.me>
2022-12-10 09:43:18 +0000

    Minimal dependencies

--- nix/core-tools/default.nix
+++ nix/core-tools/default.nix
@@ -3,20 +3,20 @@
 
 {
   buildInputs = with pkgs; [
-    vendored.postgresql
+    # vendored.postgresql
 
     git
-    gh
-    gcloud-sdk
-    python39
-    lima
-    colima
-    docker
-    azure-cli
-    kubectl
-    yq-go
-    jq
-    drone-cli
-    parallel
+    # gh
+    # gcloud-sdk
+    # python39
+    # lima
+    # colima
+    # docker
+    # azure-cli
+    # kubectl
+    # yq-go
+    # jq
+    # drone-cli
+    # parallel
   ];
 }
